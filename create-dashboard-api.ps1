4890d30244c1d7cafc7743ceedd82e1365d2081a
YO-G <yonath-g@hotmail.fr>
2023-12-22 16:52:01 +0200

    add dashboard

--- create-dashboard-api.ps1
+++ create-dashboard-api.ps1
@@ -6,23 +6,87 @@ if ($args.Length -eq 0) {
 
 $dashboardJsonFile = $args[0]
 
-# Step 1: Create Organization
-$orgResponse = Invoke-RestMethod -Uri 'http://admin:admin@localhost:3000/api/orgs' -Method Post -Headers @{"Content-Type"="application/json"} -Body '{"name":"apiorg"}'
-$orgId = $orgResponse.orgId
+# Replace 'your_admin_username' and 'your_admin_password' with valid Grafana credentials
+$adminUsername = 'admin'
+$adminPassword = 'admin'
+
+# Base64 encode the credentials
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$adminUsername"+":"+"$adminPassword"))
+
+# Set the Grafana API endpoint
+$apiUrl = 'http://localhost:3000/api/orgs'
+
+# Define the organization name
+$orgName = 'apiorg4'
+
+# Create headers with the authorization information
+$headers = @{
+    Authorization = "Basic $base64AuthInfo"
+    'Content-Type' = 'application/json'
+}
+
+# Create the body of the request
+$body = @{
+    name = $orgName
+} | ConvertTo-Json
+
+# Make the REST API call to create the organization
+try {
+    # Make the REST API call to create the organization
+    $orgResponse = Invoke-RestMethod -Uri $apiUrl -Method Post -Headers $headers -Body $body
+
+    # Extract orgId from the response
+    $orgId = $orgResponse.orgId
+    Write-Output "Organization ID: $orgId"
+}
+catch {
+    Write-Host "Failed step1 to create organization. Error: $($_.Exception.Message)"
+    exit 1
+}
 
 # Step 2: Switch org context
-Invoke-RestMethod -Uri "http://admin:admin@localhost:3000/api/user/using/$orgId" -Method Post
+try {
+    Invoke-RestMethod -Uri "http://localhost:3000/api/user/using/$orgId" -Method Post -Headers $headers
+}
+catch {
+    Write-Host "Failed step2 to switch organization context. Error: $($_.Exception.Message)"
+    exit 1
+}
 
 # Step 3: Create Service Account
-$saResponse = Invoke-RestMethod -Uri 'http://admin:admin@localhost:3000/api/serviceaccounts' -Method Post -Headers @{"Content-Type"="application/json"} -Body '{"name":"test", "role": "Admin"}'
-$saId = $saResponse.id
+try {
+    # Create the body of the request
+    $body2 = @{
+        name = "test"
+        role = "Admin"
+    } | ConvertTo-Json
+    $saResponse = Invoke-RestMethod -Uri 'http://localhost:3000/api/serviceaccounts' -Method Post -Headers $headers -Body $body2 
+    $saId = $saResponse.id
+}
+catch {
+    Write-Host "Failed step3 to create service account. Error: $($_.Exception.Message)"
+    exit 1
+}
 
 # Step 4: Create Service Account token
-$tokenResponse = Invoke-RestMethod -Uri "http://admin:admin@localhost:3000/api/serviceaccounts/$saId/tokens" -Method Post -Headers @{"Content-Type"="application/json"} -Body '{"name":"test-token"}'
-$tokenKey = $tokenResponse.key
+try {
+    $tokenResponse = Invoke-RestMethod -Uri "http://localhost:3000/api/serviceaccounts/$saId/tokens" -Method Post -Headers $headers -Body '{"name":"test-token"}'
+    $tokenKey = $tokenResponse.key
+}
+catch {
+    Write-Host "Failed step4 to create service account token. Error: $($_.Exception.Message)"
+    exit 1
+}
 
 # Step 5: Create the Dashboard from JSON File
 $keyTokenApi = $tokenKey  # Replace with the actual API key
 $dashboardContent = Get-Content $dashboardJsonFile | Out-String
 
-Invoke-RestMethod -Uri 'http://localhost:3000/api/dashboards/db' -Method Post -Headers @{"Authorization"="Bearer $keyTokenApi"; "Content-Type"="application/json"} -Insecure -Body $dashboardContent
+try {
+    Invoke-RestMethod -Uri 'http://localhost:3000/api/dashboards/db' -Method Post -Headers @{"Authorization"="Bearer $keyTokenApi"; "Content-Type"="application/json"} -Insecure -Body $dashboardContent 
+    Write-Output "Dashboard creation successful."
+}
+catch {
+    Write-Host "Failed step5 to create dashboard. Error: $($_.Exception.Message)"
+    exit 1
+}
